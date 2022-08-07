c1ba1dbba69c01935f920a519e7e146219e1f658
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-08-07 18:43:59 -0300

    tarefa: bye bye estrutura antiga

--- docs/_next/static/OsZN0Y8WMbEU3CFXQXNl2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-6187ba7c29dbb1a3.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
