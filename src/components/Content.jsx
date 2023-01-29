441f3a7727ad6224dd6376fc06c566442fd345a5
Bruno Bulnes <bulnes.bruno@gmail.com>
2023-01-29 09:42:23 -0300

    chore: restarting the project

--- src/components/Content.jsx
+++
@@ -1,9 +0,0 @@
-export function Content({ children }) {
-  return (
-    <div className="w-full max-w-5xl mx-auto px-4 lg:px-0">
-      <div className="flex flex-col md:flex-row gap-12">
-        {children}
-      </div>
-    </div>
-  )
-}
